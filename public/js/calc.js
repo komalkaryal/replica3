568bfebebf23002c8bb438b78a2f1a077d0af2f7
komalkaryal <komalkaryal6406@gmail.com>
2024-03-21 20:56:06 -0400

    save changes

--- public/js/calc.js
+++ public/js/calc.js
@@ -71,7 +71,7 @@ result.addEventListener("click", function () {
   var inputString = input.innerHTML;
 
   // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-  var numbers = inputString.split(/\+|\-|\x|\÷/g);
+  var numbers = inputString.split(/\+|\-|\x|\//g);
 
   // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
   // first we replace all the numbers and dot with empty string and then split
